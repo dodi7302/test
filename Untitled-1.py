815b53c21a6ba19af3685b342f5ebcf0e482b51f
Fikir Mohammed <fikirmohammed710@gmail.com>
2024-06-02 08:58:08 +0300

    this is the second change

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,3 +3,4 @@ if a(0) == "a" or a(0) == "e" or a(0) == "i" or a(0) == "o" or a(0) == "u" :
     print(a(0))
 if a(1) == "a" or a(1) == "e" or a(1) == "i" or a(1) == "o" or a(1) == "u" :
     print(a(1))
+    print('fikir')
