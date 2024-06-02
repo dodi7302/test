aed66c2e33c830c4c21f0f4c9f396e763b7b0114
Fikir Mohammed <fikirmohammed710@gmail.com>
2024-06-02 08:55:21 +0300

    this is the first commit

---
+++ Untitled-1.py
@@ -0,0 +1,5 @@
+a = "ab"
+if a(0) == "a" or a(0) == "e" or a(0) == "i" or a(0) == "o" or a(0) == "u" : 
+    print(a(0))
+if a(1) == "a" or a(1) == "e" or a(1) == "i" or a(1) == "o" or a(1) == "u" :
+    print(a(1))
